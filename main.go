c6e3b45950925e031da23c96eb3efe8f2e5494e4
damilolaa <oadio@tradedepot.co>
2019-04-22 03:41:41 +0100

    playing around go structs and pointers

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
 func main() {
 	// cards := newDeck()
 
@@ -14,8 +26,30 @@ func main() {
 	// cards := newDeckFromFile("my_cards")
 	// cards.print()
 
-	cards := newDeck()
-	cards.print()
-	cards.shuffle()
-	cards.print()
+	// cards := newDeck()
+	// cards.shuffle()
+
+	// alex := person{firstName: "Alex", lastName: "Goldman"}
+	// fmt.Println(alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Beglin",
+		contact: contactInfo{
+			email:   "jimbeglin@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	// jimPointer := &jim
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newName string) {
+	(*pointerToPerson).firstName = newName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
